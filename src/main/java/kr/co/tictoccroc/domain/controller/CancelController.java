bd0d01119a3b94dc7f5bd83927fda5346fd4235f
Jeongsu <tjdwlrrjatk@naver.com>
2023-11-12 00:03:29 +0900

    예약 취소 controller 입력 수정

--- src/main/java/kr/co/tictoccroc/domain/controller/CancelController.java
+++ src/main/java/kr/co/tictoccroc/domain/controller/CancelController.java
@@ -3,6 +3,7 @@ package kr.co.tictoccroc.domain.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +18,8 @@ public class CancelController {
   /**
    * 예약 취소
    */
-  @PutMapping("/book")
-  public ResponseEntity<?> cancel() {
+  @PutMapping("/book/{bookNo}")
+  public ResponseEntity<?> cancel(@PathVariable("bookNo") String bookNo) {
     return ResponseEntity.ok(null);
   }
 
---
+++ src/main/java/kr/co/tictoccroc/domain/dto/cancel/CancelReq.java
@@ -0,0 +1,10 @@
+package kr.co.tictoccroc.domain.dto.cancel;
+
+import lombok.Getter;
+
+@Getter
+public class CancelReq {
+
+  private String bookNo;
+
+}
