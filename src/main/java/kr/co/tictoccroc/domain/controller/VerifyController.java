cdade5036fae7e4ebae6ea3408f43d4f66b009e5
Jeongsu <tjdwlrrjatk@naver.com>
2023-11-12 00:56:51 +0900

    예약 현황 controller param 수정

--- src/main/java/kr/co/tictoccroc/domain/controller/VerifyController.java
+++ src/main/java/kr/co/tictoccroc/domain/controller/VerifyController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +19,7 @@ public class VerifyController {
    * 예약자 현황 (매장별)
    */
   @GetMapping("/store/{storeId}/booker")
-  public ResponseEntity<?> bookerByStore() {
+  public ResponseEntity<?> bookerByStore(@PathVariable("storeId") long storeId) {
     return ResponseEntity.ok(null);
   }
 
@@ -27,7 +28,7 @@ public class VerifyController {
    * 예약자 현황 (수업별)
    */
   @GetMapping("/lesson/{lessonId}/booker")
-  public ResponseEntity<?> bookerByLesson() {
+  public ResponseEntity<?> bookerByLesson(@PathVariable("lessonId") long lessonId) {
     return ResponseEntity.ok(null);
   }
 
@@ -36,7 +37,7 @@ public class VerifyController {
    * 예약자 현황 (매장별)
    */
   @GetMapping("/store/{storeId}/book")
-  public ResponseEntity<?> bookByStore() {
+  public ResponseEntity<?> bookByStore(@PathVariable("storeId") long storeId) {
     return ResponseEntity.ok(null);
   }
 
@@ -45,7 +46,7 @@ public class VerifyController {
    * 예약자 현황 (수업별)
    */
   @GetMapping("/lesson/{lessonId}/book")
-  public ResponseEntity<?> bookByLesson() {
+  public ResponseEntity<?> bookByLesson(@PathVariable("lessonId") long lessonId) {
     return ResponseEntity.ok(null);
   }
 
