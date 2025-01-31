ddcc35dfd95d5f32eddd100973117b03657310ac
Jeongsu <tjdwlrrjatk@naver.com>
2025-01-31 13:01:37 +0900

    테스트

--- src/main/java/kr/co/moin/global/enumeration/encryptAndDecrypt/Algorithm.java
+++ src/main/java/kr/co/moin/global/enumeration/encryptAndDecrypt/Algorithm.java
@@ -9,9 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 
 import javax.crypto.Cipher;
-import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
-import java.nio.charset.StandardCharsets;
 import java.text.Normalizer;
 import java.util.Base64;
 
@@ -53,57 +51,6 @@ public enum Algorithm {
         return new String(cipher.doFinal(Base64.getDecoder().decode(str)));
       } catch (Exception e) {
         log.info("str: {}, key: {}, error: {}", str, key, e.getMessage());
-        throw new GlobalException(ErrorCode.ENCRYPTION_DECRYPTION_ERROR);
-      }
-    }
-  },
-
-  /**
-   * AES/CBC/PKCS5Padding 암복호화 로직
-   */
-  AES_CBC_PKCS5Padding("AES/CBC/PKCS5Padding") {
-    @Override
-    public String encode(String str, String key) {
-      if (StringUtils.isBlank(str)) {
-        return "";
-      }
-
-      try {
-        Cipher cipher = getCipher(key.getBytes(StandardCharsets.UTF_8), Cipher.ENCRYPT_MODE);
-        return Base64.getEncoder().encodeToString(cipher.doFinal(str.getBytes(StandardCharsets.UTF_8)));
-      } catch (Exception e) {
-        log.info("str: {}, key: {}, error: {}", str, key, e.getMessage());
-        throw new GlobalException(ErrorCode.ENCRYPTION_DECRYPTION_ERROR);
-      }
-    }
-
-    @Override
-    public String decode(String str, String key) {
-      if (StringUtils.isBlank(str)) {
-        return "";
-      }
-
-      try {
-        Cipher cipher = getCipher(key.getBytes(StandardCharsets.UTF_8), Cipher.DECRYPT_MODE);
-        return new String(cipher.doFinal(Base64.getDecoder().decode(str)), StandardCharsets.UTF_8);
-      } catch (Exception e) {
-        log.info("str: {}, key: {}, error: {}", str, key, e.getMessage());
-        throw new GlobalException(ErrorCode.ENCRYPTION_DECRYPTION_ERROR);
-      }
-    }
-
-    private Cipher getCipher(byte[] keyByte, int mode) {
-      try {
-        Cipher cipher = Cipher.getInstance(this.getAlgorithm());
-        byte[] IV = new byte[16];
-        SecretKeySpec secretKeySpec = new SecretKeySpec(keyByte, "AES");
-        IvParameterSpec ivParameterSpec = new IvParameterSpec(IV);
-
-        cipher.init(mode, secretKeySpec, ivParameterSpec);
-
-        return cipher;
-      } catch (Exception e) {
-        log.info("keyByte: {}, mode: {}, error: {}", keyByte, mode, e.getMessage());
         throw new GlobalException(ErrorCode.ENCRYPTION_DECRYPTION_ERROR);
       }
     }
